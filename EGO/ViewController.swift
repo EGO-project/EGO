b9350d25752dc70edbd547ecd9b57481af36a5f1
GangsangMSK <wjdgudtk1234@gmail.com>
2023-01-25 12:48:22 +0900

    Initial Commit

---
+++ EGO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EGO
+//
+//  Created by 김민석 on 2023/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
