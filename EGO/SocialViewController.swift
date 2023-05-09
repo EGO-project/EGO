21dad8c395a180e1057d35a35cf9c597a0947deb
Jaeha Hwang <wogk216@naver.com>
2023-05-09 23:52:24 +0900

    친구 파일 수정

--- EGO/SocialViewController.swift
+++ EGO/SocialViewController.swift
@@ -11,8 +11,8 @@ import FirebaseDatabase
 
 class SocialViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var nameList: [String] = ["친구1","친구2","친구3","친구4"]
-    var egoList : [String] = ["다람쥐.png", "사자.png", "수달.png", "코알라.png"]
+    var nameList: [String] = ["친구1","친구2","친구3"]
+    var egoList : [String] = ["egg_다람쥐.png", "egg_사자.png", "egg_수달.png"]
     
     // 파이어베이스 주소
     let ref = Database.database().reference()
